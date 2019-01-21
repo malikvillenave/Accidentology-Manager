fe008d56d0c4ce95e62581f8f6189cbff59944c4
malik villenave <malik.villenave@gmail.com>
2019-01-21 14:33:43 +0100

    Sauvegarde des tests de parallélisation du backend dans un autre répertoire/fichier

--- BackEnd/BackEnd/app.py
+++ BackEnd/BackEnd/app.py
@@ -5,7 +5,6 @@ import math
 from numpy import mean
 import config
 import time
-import multiprocessing as mp
 from threading import Thread, Semaphore
 import queue
 
@@ -20,6 +19,7 @@ except:
 app.config['DEBUG'] = True
 
 api = Api(app)
+sem = Semaphore()
 
 # rqt a modifier/utiliser plustard
 # rqt = "SELECT indicateur " \
@@ -49,251 +49,28 @@ def create_indicator_request(first_waypoint,second_waypoint):
     return rqt
 
 
-class ServiceIndicator(Resource):
-    def get(self):
-        try:
-            json = request.json['response']
-            if json is None:
-                return {"post": []}
-            waypoint_interval = 100
-
-            for indexRoute, route in enumerate(request.json['response']['route']):
-                waypoints = route['shape']
-                moy_indicator = []
-                for index, waypoint in enumerate(waypoints):
-                    if index > len(waypoints) - waypoint_interval:
-                        break
-
-                    if index % waypoint_interval == 0:
-                        rqt = create_indicator_request(waypoint, waypoints[index + waypoint_interval])
-                        cursor.execute(rqt)
-                        for record in cursor:
-                            if record[0]:
-                                moy_indicator.append(record[0])
-                route['dangerLevel'] = mean(moy_indicator)
-                json['route'][indexRoute] = route
-
-            return {"response": json}
-        except Exception as e:
-            print(e)
-            return {"response": {}}, 404
-
-    def post(self):
-        try:
-            json = request.json['response']
-            if json is None:
-                return {"post": []}, 405
-            waypoint_interval = 100
-
-            for indexRoute, route in enumerate(request.json['response']['route']):
-                waypoints = route['shape']
-                moy_indicator = []
-                for index, waypoint in enumerate(waypoints):
-                    if index > len(waypoints) - waypoint_interval:
-                        break
-
-                    if index % waypoint_interval == 0:
-                        rqt = create_indicator_request(waypoint, waypoints[index + waypoint_interval])
-                        cursor.execute(rqt)
-                        for record in cursor:
-                            if record[0]:
-                                moy_indicator.append(record[0])
-                route['dangerLevel'] = mean(moy_indicator)
-                json['route'][indexRoute] = route
-
-            print(json)
-
-            return {"response": json}
-        except Exception as e:
-            print(e)
-            return {"response": {}}, 404
-
-    def delete(self):
-        return {"delete": "example"}
-
-    def put(self):
-        return {"put": "example"}
-
-
-class ServiceIndicatorTimeTest(Resource):
-    def get(self):
-        return {"get": "example"}
-
-    def post(self):
-        try:
-            start = time.time()
-            json = request.json['response']
-            if json is None:
-                return {"post": []}, 405
-            waypoint_interval = 100
-
-            for indexRoute, route in enumerate(request.json['response']['route']):
-                waypoints = route['shape']
-                moy_indicator = []
-                for index, waypoint in enumerate(waypoints):
-                    if index > len(waypoints) - waypoint_interval:
-                        break
-
-                    if index % waypoint_interval == 0:
-                        rqt = create_indicator_request(waypoint, waypoints[index + waypoint_interval])
-                        cursor.execute(rqt)
-                        for record in cursor:
-                            if record[0]:
-                                moy_indicator.append(record[0])
-                route['dangerLevel'] = mean(moy_indicator)
-                json['route'][indexRoute] = route
-
-            end = time.time()
-            print(end - start)
-
-            return {"response": json}
-        except Exception as e:
-            print(e)
-            return {"response": {}}, 404
-
-    def delete(self):
-        return {"delete": "example"}
-
-    def put(self):
-        return {"put": "example"}
-
-
-def processRoute(route) :
-    waypoint_interval = 100 #place temporairement ici
-    waypoints = route['shape']
-    moyIndicator = []
-    for index, waypoint in enumerate(waypoints):
-        if index > len(waypoints) - waypoint_interval:
-            break
-
-        if index % waypoint_interval == 0:
-            rqt = create_indicator_request(waypoint, waypoints[index + waypoint_interval])
-            cursor.execute(rqt)
-            for record in cursor:
-                if record[0]:
-                    moyIndicator.append(record[0])
-    dangerLevel = mean(moyIndicator)
-    return dangerLevel
-
-
-class ServiceIndicatorMPTest(Resource):
-    def get(self):
-        return {"get": "example"}
-
-    def post(self):
-        try:
-            start = time.time()
-            json = request.json['response']
-            if json is None:
-                return {"post": []}, 405
-            pool = mp.Pool(processes=3)
-            results = [pool.apply(processRoute, args=(r,)) for r in request.json['response']['route']]
-            for i, dglev in enumerate(results):
-                json['route'][i]['dangerLevel'] = dglev
-
-            end = time.time()
-            print(end - start)
-            return {"response": json}
-        except Exception as e:
-            print(e)
-            return {"response": {}}, 404
-
-    def delete(self):
-        return {"delete": "example"}
-
-    def put(self):
-        return {"put": "example"}
-
-
-sem = Semaphore()
-
-
-def processRouteQueue(route,q,number):
-    waypoint_interval = 100 #place temporairement ici
-    waypoints = route['shape']
-    moyIndicator = []
-    for index, waypoint in enumerate(waypoints):
-        if index > len(waypoints) - waypoint_interval:
-            break
-
-        if index % waypoint_interval == 0:
-            rqt = create_indicator_request(waypoint, waypoints[index + waypoint_interval])
-            sem.acquire()
-            cursor.execute(rqt)
-            for record in cursor:
-                if record[0]:
-                    moyIndicator.append(record[0])
-            sem.release()
-    dangerLevel = mean(moyIndicator)
-    q.put((number, dangerLevel))
-
-
-class ServiceIndicatorThreadTest(Resource):
-    def get(self):
-        return {"get": "example"}
-
-    def post(self):
-        try:
-            start = time.time()
-
-            resQueue = queue.Queue()
-
-            json = request.json['response']
-            if json is None:
-                return {"post": []}, 405
-            waypoint_interval = 100
-
-            threads=[]
-
-            for indexRoute, route in enumerate(request.json['response']['route']):
-                t = Thread(target=processRouteQueue, args=(route, resQueue, indexRoute))
-                t.start()
-                threads.append(t)
-
-            for t in threads:
-                t.join()
-
-            while not resQueue.empty():
-                result = resQueue.get()
-                json['route'][result[0]]['dangerLevel'] = result[1]
-
-            end = time.time()
-            print(end - start)
-
-            return {"response": json}
-        except Exception as e:
-            print(e)
-            return {"response": {}}, 404
-
-    def delete(self):
-        return {"delete": "example"}
-
-    def put(self):
-        return {"put": "example"}
-
-
 def processWaypointQueue(waypoint,waypoints,q,index):
     waypoint_interval = 100 #place temporairement ici
     rqt = create_indicator_request(waypoint, waypoints[index + waypoint_interval])
-    dangerLevel = 0
+    danger_level = 0
     sem.acquire()
     cursor.execute(rqt)
     for record in cursor:
         if record[0]:
-            dangerLevel = record[0]
+            danger_level = record[0]
     sem.release()
-    q.put(dangerLevel)
+    q.put(danger_level)
 
 
-class ServiceIndicatorThreadTest2(Resource):
+class ServiceIndicator(Resource):
     def get(self):
-        return {"get": "example"}
+        return {"get": "not implemented"}
 
     def post(self):
         try:
             start = time.time()
 
-            resQueue = queue.Queue()
+            res_queue = queue.Queue()
 
             json = request.json['response']
             if json is None:
@@ -311,21 +88,20 @@ class ServiceIndicatorThreadTest2(Resource):
                         break
 
                     if index % waypoint_interval == 0:
-                        t = Thread(target=processWaypointQueue, args=(waypoint, waypoints, resQueue, index))
+                        t = Thread(target=processWaypointQueue, args=(waypoint, waypoints, res_queue, index))
                         t.start()
                         threads.append(t)
 
                 for t in threads:
                     t.join()
 
-                while not resQueue.empty():
-                    result = resQueue.get()
+                while not res_queue.empty():
+                    result = res_queue.get()
                     moy_indicator.append(result)
 
                 json['route'][indexRoute]['dangerLevel'] = mean(moy_indicator)
 
             end = time.time()
-            print(end - start)
 
             return {"response": json}
         except Exception as e:
@@ -333,16 +109,12 @@ class ServiceIndicatorThreadTest2(Resource):
             return {"response": {}}, 404
 
     def delete(self):
-        return {"delete": "example"}
+        return {"delete": "not implemented"}
 
     def put(self):
-        return {"put": "example"}
+        return {"put": "not implemented"}
 
 
 api.add_resource(ServiceIndicator, '/Indicator')
-api.add_resource(ServiceIndicatorTimeTest, '/IndicatorTime')
-api.add_resource(ServiceIndicatorMPTest, '/IndicatorMP')
-api.add_resource(ServiceIndicatorThreadTest, '/IndicatorThread')
-api.add_resource(ServiceIndicatorThreadTest2, '/IndicatorThread2')
 if __name__ == '__main__':
     app.run()
